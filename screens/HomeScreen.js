070c222378e68ade1f1f943cd1983a4dbb205b93
Maria Sole Cincis <m.cincis@ikusei.de>
2020-04-03 16:58:10 +0200

    Debug on form submission
    - deleted scrollView from react-native-gesture-handler because I think i'm not going to use it
    - moved ALL_EXPENSES out of my component because :
      - first it needs to be declared before I create the expenses state
      - second it doesn't belong to the component , it is just hardcoded data that I'm using now for testing but I'll deleted later
    - deleted comments despite their utility( I want to pack them in issues )
    -textList data is now rendering expenses which is a dynamic , that means every time that I setExpenses I also update my list on the screen as well
    - deleted finally all the useless styles

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,39 +2,31 @@ import React, { useState } from 'react';
 import { Button, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Input } from 'react-native-elements';
 import _sumBy from 'lodash/sumBy';
-import { ScrollView } from 'react-native-gesture-handler';
+
 import * as WebBrowser from 'expo-web-browser';
 
 import { MonoText } from '../components/StyledText';
 import { TextList } from '../components';
 
+const ALL_EXPENSES = [
+  { id: 1, name: 'Buy a book', amount: 20 },
+  { id: 2, name: 'Buy dog food', amount: 5 },
+  { id: 3, name: 'Book a flight ticket', amount: 225 },
+  { id: 4, name: 'Buy speakers', amount: 300 },
+  { id: 5, name: 'Book a concert ticket', amount: 50 },
+  { id: 6, name: 'Buy birthday present', amount: 100 },
+  { id: 7, name: 'Buy boots', amount: 400 },
+  { id: 7, name: 'Buy boots', amount: 400 }
+];
 export default function HomeScreen() {
   const [expenses, setExpenses] = useState(ALL_EXPENSES);
   const [name, setName] = useState('');
   const [amount, setAmount] = useState('');
 
-  const ALL_EXPENSES = [
-    { id: 1, name: 'Buy a book', amount: 20 },
-    { id: 2, name: 'Buy dog food', amount: 5 },
-    { id: 3, name: 'Book a flight ticket', amount: 225 },
-    { id: 4, name: 'Buy speakers', amount: 300 },
-    { id: 5, name: 'Book a concert ticket', amount: 50 },
-    { id: 6, name: 'Buy birthday present', amount: 100 },
-    { id: 7, name: 'Buy boots', amount: 400 }
-  ];
-
-  let totalExpense = _sumBy(ALL_EXPENSES, 'amount');
-
   const handleSubmitForm = () => {
-    //check whether the name is not empty and the amount is not negative
     if (name !== '' && amount > 0) {
-      // single expense object
       const expense = { name, amount };
-      // do not override previous values in the array
-      // use spread operator to access previous values
-      setExpenses([...expenses, expense]); // !!! INVALID ATTEMPT TO SPREAD NON-ITERABLE INSTANCE.
-
-      // clean input fields
+      setExpenses([...expenses, expense]);
       setName('');
       setAmount('');
     } else {
@@ -67,14 +59,14 @@ export default function HomeScreen() {
       </View>
 
       <View style={styles.flexContainer}>
-        <TextList style={styles.listContainer} noBottomDivider data={ALL_EXPENSES} />
+        <TextList style={styles.listContainer} noBottomDivider data={expenses} />
       </View>
 
       <View style={styles.tabBarInfoContainer}>
         <Text style={styles.tabBarInfoText}>Total Expense is :</Text>
 
         <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>{totalExpense}</MonoText>
+          <MonoText style={styles.codeHighlightText}>{_sumBy(expenses, 'amount')}</MonoText>
         </View>
       </View>
     </View>
@@ -93,28 +85,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
-
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center'
-  },
-  contentContainer: {
-    paddingTop: 30
-  },
   welcomeContainer: {
     alignItems: 'center',
     marginTop: 10,
     marginBottom: 20
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10
   },
   getStartedContainer: {
     alignItems: 'center',
@@ -122,20 +96,6 @@ const styles = StyleSheet.create({
   },
   homeScreenFilename: {
     marginVertical: 7
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)'
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center'
   },
   tabBarInfoContainer: {
     position: 'absolute',
@@ -164,16 +124,5 @@ const styles = StyleSheet.create({
   },
   navigationFilename: {
     marginTop: 5
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center'
-  },
-  helpLink: {
-    paddingVertical: 15
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7'
   }
 });
