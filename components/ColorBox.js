6566559fccb513a6b385a85083a68265ae94cb04
Maria Sole Cincis <msc@tpwd.de>
2021-08-17 16:43:37 +0200

    feat(styles): created color box component
    - implemented styled components
    - colorBox with 4 props
    - implemented colorBox in ColorGeneratorScreen

---
+++ components/ColorBox.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import styled, { css } from 'styled-components';
+
+
+export const ColorBox = styled.View`
+    flex: 1;
+    maxHeight: 50px;
+    maxWidth: 50px;
+
+  ${(props) =>
+    props.red &&
+    css`
+     background-color: rgb(255, 0, 0);
+    `};
+    ${(props) =>
+    props.green &&
+    css`
+     background-color: rgb(0, 255, 0);
+    `};
+    ${(props) =>
+    props.blu &&
+    css`
+     background-color: rgb(0, 0, 255);
+    `};
+    ${(props) =>
+    props.generated &&
+    css`
+     background-color: rgb(0, 0, 0);
+    `};
+`;
