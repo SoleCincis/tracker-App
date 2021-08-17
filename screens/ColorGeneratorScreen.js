3aede862599b839b47f1ea653bd57d6448a16264
Maria Sole Cincis <msc@tpwd.de>
2021-08-17 18:49:21 +0200

    feat(button): implemented button component
    -first steps plus and minus logics

--- screens/ColorGeneratorScreen.js
+++ screens/ColorGeneratorScreen.js
@@ -2,19 +2,47 @@ import * as React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-import { ColorBox } from '../components';
+import { Button, ColorBox } from '../components';
 
 export default function ColorGeneratorScreen() {
 
+  const plusTen = (value) => {
+  !0 <= value >= 255
+    let increasedValue = value + 10
+    return increasedValue
 
+  };
+
+  const minusTen = (value) => {
+    !0 <= value >= 255
+    let decreased = value - 10
+    return decreased
+
+  };
 
   return (
 
 
     <View style={styles.colorsContainer}>
       <ColorBox red />
+      <View style={styles.buttonsContainer}>
+        <Button title="-10" onPress={minusTen} />
+        <Button title="+10" onPress={plusTen} />
+      </View>
       <ColorBox green />
+      <View style={styles.buttonsContainer}>
+        <Button title="-10" onPress />
+        <Button title="+10" onPress />
+      </View >
       <ColorBox blu />
+      <View style={styles.buttonsContainer}>
+        <Button title="-10" onPress />
+        <Button title="+10" onPress />
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button title="random value" />
+        <Button title="generated color" />
+      </View>
     </View>
 
 
@@ -27,8 +55,14 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'space-around',
-    backgroundColor: '#fafafa',
+    backgroundColor: 'rgb(221, 221, 221)',
     paddingHorizontal: 100,
     marginHorizontal: 30,
-  }
+  },
+  buttonsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
 });
