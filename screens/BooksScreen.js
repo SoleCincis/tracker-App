fe844adcbd5bf5a8ef5c022ee3cd08e4955eba3a
Maria Sole Cincis <msc@tpwd.de>
2021-11-01 17:22:01 +0100

    feat(BooksScreen): showing data on list
    - showing first author + number of more authors if there is some
    - isbn still not visible on screen
    - add some style to check for better readability

--- screens/BooksScreen.js
+++ screens/BooksScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function BooksScreen() {
   const [isLoading, setLoading] = useState(true);
@@ -21,28 +21,69 @@ export default function BooksScreen() {
     getBooks();
   }, []);
 
-  const authorName = data.map((e) => {
-    e.authors.map((author) => {
-      console.log(author.name)
-      return author.name
-    })
-     });
+  //looking for ISBN
+  const availabilities =  data.map((book) => {
+    return !!book && book.availability
+  });
+
+  const isbnArray = availabilities.map((availability)=>{
+    console.log("availability.isbn:", availability !== undefined ? availability.isbn : '')
+    return availability !== undefined ? availability.isbn : ''
+  })
+    console.log("isbnArray:", isbnArray)
+
+  const isbn = isbnArray.forEach((isbn) => {
+    console.log("ISBN:",  typeof isbn === "string" ? isbn : '')
+    return typeof isbn === "string" ? `[ISBN: ${isbn}]` : ''
+  })
+  console.log("constISBN:", isbn)
+
+  const renderItem = ({ item }) => {
+
+    const authors = item.authors.length > 1
+      ? `[authors: ${item.authors[0].name} and ${item.authors.length - 1} more]`
+      : `[author: ${item.authors[0].name}]`
+      // console.log(item.authors.length)
+
+    return (
+      <View>
+        <Text style={styles.item}>
+          {item.title} {authors} {isbn}
+        </Text>
+
+        {/* {isbnArray.forEach(isbn =>
+          <Text>
+            {typeof isbn === "string" ? isbn : '' }
+          </Text>
+        )} */}
+
+      </View>
+    );
+  };
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator /> : (
 
-      <FlatList
+      {isLoading ? <ActivityIndicator /> : (
+        <FlatList
           data={data}
           key={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text>
-              {item.title} [authors: { item.authors[0].name } and {item.authors.length - 1} more authors]
-            </Text>
-          )}
+          renderItem={renderItem}
         />
-
       )}
+
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    backgroundColor: '#08bece',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  }
+});
